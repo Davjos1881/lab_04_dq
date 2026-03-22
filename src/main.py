f81796d529934eef033474dbc533b6a2f6ac62e3
brayan <brayantigreros2021@gmail.com>
2026-03-22 00:22:53 -0500

    Validate input hecho, documentado data docs de GE

--- src/main.py
+++ src/main.py
@@ -9,20 +9,27 @@ import re
 from pathlib import Path
 from log import log_progress
 from extract import extract_raw_data, data_profiling
+from validate_input import input_data_validation
 
 base_path = Path(r"C:\Users\btigr\Documents\UAO\5\ETL\ETL_2026_1\lab_04\lab_04_dq-main\lab_04_dq-main")
 
-raw_file      = base_path / "data" / "raw" / "retail_etl_dataset.csv"
-log_file      = base_path / "logs" / "log_file.txt"
+raw_file = base_path / "data" / "raw" / "retail_etl_dataset.csv"
+log_file = base_path / "logs" / "log_file.txt"
+inp_validate = base_path / "reports" 
 
 def main():
 
     log_progress("Starting ETL process", log_file)
 
-    log_progress("Extract and Profiling phase started", log_file)
+    log_progress("Extract phase started", log_file)
 
     df_raw = extract_raw_data(raw_file)
-    df_profiling = data_profiling(df_raw)
+
+    log_progress("Data profiling", log_file)
+    data_profiling(df_raw)
+
+    log_progress("Input validation data", log_file)
+    input_data_validation(df_raw, inp_validate)
     
 
 
---
+++ src/validate_input.py
@@ -0,0 +1,90 @@
+import great_expectations as gx
+import pandas as pd
+
+
+def input_data_validation(df_raw, inp_validate):
+    
+    # Get the Ephemeral Data Context
+    context = gx.get_context(mode="file", project_root_dir=str(inp_validate))
+
+    # Add a Pandas Data Source
+    data_source_name = "retail_etl_dataset"
+    data_source = context.data_sources.add_pandas(name=data_source_name)
+
+    # Add a Data Asset to the Data Source
+    data_asset_name = "retail_etl_dataset_asset"
+    data_asset = data_source.add_dataframe_asset(name=data_asset_name)
+
+    # Define the Batch Definition name
+    batch_definition_name = "retail_etl_dataset_batch"
+    batch_definition = data_asset.add_batch_definition_whole_dataframe(batch_definition_name)
+
+    # Accuracy - columna auxiliar sobre df_raw
+    df_raw["revenue_match"] = (
+        abs(df_raw["total_revenue"] - df_raw["quantity"] * df_raw["price"]) <= 0.01
+    )
+
+    # Pasar df_raw al batch
+    batch_parameters = {"dataframe": df_raw}
+    batch = batch_definition.get_batch(batch_parameters=batch_parameters)
+
+    # Create an Expectation Suite
+    expectation_suite_name = "retail_etl_dataset_suite"
+    suite = gx.ExpectationSuite(name=expectation_suite_name)
+
+    # Completeness
+    suite.add_expectation(
+        gx.expectations.ExpectColumnValuesToNotBeNull(column="customer_id")
+    )
+    suite.add_expectation(
+        gx.expectations.ExpectColumnValuesToNotBeNull(column="invoice_date")
+    )
+
+    # Uniqueness
+    suite.add_expectation(
+        gx.expectations.ExpectColumnValuesToBeUnique(column="invoice_id")
+    )
+
+    # Validity
+    suite.add_expectation(
+        gx.expectations.ExpectColumnValuesToBeBetween(column="quantity", min_value=1)
+    )
+    suite.add_expectation(
+        gx.expectations.ExpectColumnValuesToBeBetween(column="price", min_value=0.01)
+    )
+    suite.add_expectation(
+        gx.expectations.ExpectColumnValuesToBeInSet(column="product", value_set=["Headphones", "Keyboard", "Mouse", "Phone", "Laptop", "Printer", "Monitor", "Tablet"])
+    )
+
+    # Accuracy
+    suite.add_expectation(
+        gx.expectations.ExpectColumnValuesToBeInSet(
+            column="revenue_match",
+            value_set=[True]
+        )
+    )
+
+    # Consistency
+    suite.add_expectation(
+        gx.expectations.ExpectColumnValuesToBeInSet(column="country", value_set=["Colombia", "Chile", "Peru", "Ecuador"])
+    )
+
+    # Timeliness
+    suite.add_expectation(
+        gx.expectations.ExpectColumnValuesToMatchRegex(column="invoice_date", regex=r"^\d{4}-\d{2}-\d{2}$")
+    )
+    suite.add_expectation(
+        gx.expectations.ExpectColumnValuesToBeBetween(column="invoice_date", min_value="2023-01-01", max_value="2023-12-31")
+    )
+
+    # Add the Expectation Suite to the Context
+    context.suites.add(suite)
+
+    # Validate
+    validation_results = batch.validate(suite)
+    print(validation_results)
+
+    # Generar Data Docs
+    context.build_data_docs()
+    context.open_data_docs()
+    
